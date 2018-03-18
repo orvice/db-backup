72615d168f383ad7714da52ac9e69e47969590f6
orvice <orvice@gmail.com>
2018-03-18 18:27:58 +0800

    init

---
+++ env.go
@@ -0,0 +1,25 @@
+package main
+
+import "github.com/orvice/utils/env"
+
+var(
+	S3_Key,S3_Secret,S3_Region,S3_Bucket string
+
+	DB_Host,DB_User,DB_Password,DB_Port,DB_DB string
+
+	Telegram_Token string
+	Telegram_ChatID int
+)
+
+func InitEnv(){
+	S3_Key = env.Get("S3_KEY")
+	S3_Secret = env.Get("S3_SECRET")
+	S3_Region = env.Get("S3_REGION")
+	S3_Bucket = env.Get("S3_BUCKET")
+
+	DB_Host = env.Get("DB_HOST")
+	DB_User = env.Get("DB_USER")
+	DB_Password = env.Get("DB_PASSWORD")
+	DB_Port = env.Get("DB_PORT")
+	DB_DB = env.Get("DB_DB")
+}
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"time"
+
+	"github.com/lukerodham/barkup"
+	"github.com/sirupsen/logrus"
+	"github.com/orvice/kit/log"
+)
+
+var(
+	logger log.Logger
+)
+
+func backup() {
+
+	// Configure a MySQL exporter
+	mysql := &barkup.MySQL{
+		Host:     DB_Host,
+		Port:     DB_Port,
+		DB:       DB_DB,
+		User:     DB_User,
+		Password: DB_Password,
+	}
+
+	// Configure a S3 storer
+	s3 := &barkup.S3{
+		Region:       S3_Region,
+		Bucket:       S3_Bucket,
+		AccessKey:    S3_Key,
+		ClientSecret: S3_Secret,
+	}
+
+	// Export the database, and send it to the
+	// bucket in the `db_backups` folder
+	err := mysql.Export(false).To("db_backups/", s3)
+	if err != nil {
+		logrus.Error(err)
+	}
+}
+
+func main() {
+	logger = log.NewDefaultLogger()
+	InitEnv()
+	for {
+		backup()
+		time.Sleep(time.Minute * 10)
+	}
+}
